e86e9c87fd04d97f375fb500dbe37c86609bb7c1
hozuxx03 <y.m.m.s.xxx@gmail.com>
2023-10-02 15:06:56 +0900

    ヴィジェットを作成

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import  Timeline  from './components/timeline/Timeline.js';
 import './App.css';
 import Sideber from './components/sidebar/Sideber';
+import Widgets from './components/widget/Widgets.js';
+
 
 
 function App() {
@@ -8,12 +10,10 @@ function App() {
     <div className="app">
       {/* Sidebar */}
       <Sideber />
-
       {/* Timeline */}
       <Timeline />
-
-
       {/* Widget */}
+      <Widgets />
     </div>
   );
 }
