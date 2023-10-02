1b352042d0e6b06e5966c4474c76fe4e14933076
hozuxx03 <y.m.m.s.xxx@gmail.com>
2023-10-02 15:49:22 +0900

    Widgets.css追加

--- src/components/widget/Widgets.js
+++ src/components/widget/Widgets.js
@@ -1,11 +1,16 @@
 import { Search } from '@mui/icons-material';
 import React from 'react';
+import { TwitterTimelineEmbed,
+        TwitterShareButton,
+        TwitterTweetEmbed,
+        } from 'react-twitter-embed';
+import "./Widgets.css";
 
 function Widgets() {
     return (
     <div className='widgets'>
         <div className='widgets--input'>
-            <Search className='widgets--search--Icon'/>
+            <Search className='widgets--searchIcon'/>
             <input placeholder='キーワード検索' type='text'/>
         </div>
 
@@ -13,7 +18,18 @@ function Widgets() {
             <h2>いまどうしてる？</h2>
 
             {/*ライブラリを追加・追記*/}
-            
+            <TwitterTweetEmbed tweetId={'1475764828208009220'}/>
+
+            <TwitterTimelineEmbed
+                sourceType='profile'
+                screenName='Shin_Enginner'
+                options={{ height :400 }}
+            />
+            <TwitterShareButton
+                url={'https://twitter.com/Shin_Engineer'}
+                options={{ text: '#React.js 勉強中',
+                via: 'Shin_Engineer'}}
+            />
         </div>
     </div>
     );
