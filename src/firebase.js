8490f1aecc3a85d131dffa6af3789aeab74e2ba6
hozuxx03 <y.m.m.s.xxx@gmail.com>
2023-10-06 15:22:31 +0900

    firebase初期化設定

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA4fLLKN27YgmTsMFA4xZDbXmLjzdbE7Hw",
+    authDomain: "twitter-clone-udemy-d5192.firebaseapp.com",
+    projectId: "twitter-clone-udemy-d5192",
+    storageBucket: "twitter-clone-udemy-d5192.appspot.com",
+    messagingSenderId: "287834632602",
+    appId: "1:287834632602:web:cbd6b2e1986c65a02d4a23"
+};
+
+const app = initializeApp(firebaseConfig);
